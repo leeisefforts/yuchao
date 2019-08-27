b38316d4c8eab02070c23bd8e19a5dd9f00c42cd
Bryant Lee <leeisefforts@163.com>
2019-08-27 19:27:02 +0800

    aaa

---
+++ yuchao/yuchao/Controllers/Admin/Club/ClubController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yuchao.Controllers.Admin.Club
+{
+    /// <summary>
+    /// 俱乐部后台接口
+    /// </summary>
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class ClubController: Controller
+    {
+        /// <summary>
+        /// 获取俱乐部列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult Get() {
+            return Json("Success");
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody]string values) {
+            return Json("Success");
+        }
+    }
+}
